f5455c02cf4604d0bb9700f5e8c1379951076a7a
Dina <diana.liubarets@hotmail.com>
2020-10-04 16:42:04 -0400

    Updates

---
+++ precache-manifest.c57c6acaee34b52c1761c402b4c17c3b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1cab608ae2e3a0eb094d91770b76d7",
+    "url": "/React-bootstrap-portfolio/index.html"
+  },
+  {
+    "revision": "40564227b01a0f3734d5",
+    "url": "/React-bootstrap-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "327b426c172e16196eaf",
+    "url": "/React-bootstrap-portfolio/static/css/main.39c88a3b.chunk.css"
+  },
+  {
+    "revision": "40564227b01a0f3734d5",
+    "url": "/React-bootstrap-portfolio/static/js/2.db6ec3e7.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "/React-bootstrap-portfolio/static/js/2.db6ec3e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327b426c172e16196eaf",
+    "url": "/React-bootstrap-portfolio/static/js/main.1c4e91b9.chunk.js"
+  },
+  {
+    "revision": "6a12fad99f1b7ac1897e",
+    "url": "/React-bootstrap-portfolio/static/js/runtime-main.7bad5d57.js"
+  },
+  {
+    "revision": "97e616cc99de779069e2e32d3a245019",
+    "url": "/React-bootstrap-portfolio/static/media/about.97e616cc.jpeg"
+  },
+  {
+    "revision": "abc8753f598ded617ef4223435dd870a",
+    "url": "/React-bootstrap-portfolio/static/media/appoint.abc8753f.png"
+  },
+  {
+    "revision": "a8ad463248554d21cf2daeae07589510",
+    "url": "/React-bootstrap-portfolio/static/media/burger.a8ad4632.png"
+  },
+  {
+    "revision": "e997ce52760ca5c44a28ce64878914da",
+    "url": "/React-bootstrap-portfolio/static/media/crypto.e997ce52.png"
+  },
+  {
+    "revision": "b5e762dea577f393f742750a4d1eb304",
+    "url": "/React-bootstrap-portfolio/static/media/main1.b5e762de.jpeg"
+  },
+  {
+    "revision": "877510dccc51d796349474a4cbd08dce",
+    "url": "/React-bootstrap-portfolio/static/media/seberianbuckwheat.877510dc.png"
+  },
+  {
+    "revision": "f087294e5a272d7b2a4feeb1be903501",
+    "url": "/React-bootstrap-portfolio/static/media/todolist.f087294e.png"
+  },
+  {
+    "revision": "c7d81f746fa44d014697a7c685626b71",
+    "url": "/React-bootstrap-portfolio/static/media/unheatedhoney.c7d81f74.png"
+  }
+]);
